cf7974cbcc477228fe26473ca049c6be422d41bd
javajigi <javajigi@gmail.com>
2022-12-26 15:55:24 +0900

    add static web files

---
+++ src/main/java/camp/nextstep/qna/QnaApplication.java
@@ -0,0 +1,13 @@
+package camp.nextstep.qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/camp/nextstep/qna/QnaApplicationTests.java
@@ -0,0 +1,13 @@
+package camp.nextstep.qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
